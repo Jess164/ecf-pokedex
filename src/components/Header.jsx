3d8d8b7fa50ca2abdc706becbd3e12ef5174dafb
jessica <mbumba.jessica11@gmail.com>
2025-06-14 23:35:18 +0200

    feat(header): add header layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import { Logo } from "./Logo"
+
+export function Header() {
+    return <div className="bg-[#FFF1E0] py-4 md:px-4 flex justify-center md:justify-start">
+        <Logo/>
+    </div>
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,6 @@
+export function Logo() {
+    return <div className="flex items-center gap-1">
+        <img src="/public/pokeball.png" alt="" className="w-5 h-auto"/>
+        <img src="/public/pokedex.png" alt="Logo du site Pokédex" className="w-25 h-auto" />
+    </div>
+}
